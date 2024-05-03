c9da7b843bc78d85d1db15fc6caec3d6e75f553b
shabbeer1993 <shabbeerjntua786@gmail.com>
2024-05-03 09:12:17 +0530

    creating object using new object syntax

--- app.js
+++ app.js
@@ -212,136 +212,167 @@
 
 //JavaScript is  Dynamic Language
 
-String;
+// String;
 
-let x = "Session With Siva";
-console.log(x);
-console.log(typeof x);
+// let x = "Session With Siva";
+// console.log(x);
+// console.log(typeof x);
 
-x = 1;
+// x = 1;
 
-// Number
-let y = 1;
+// // Number
+// let y = 1;
 
-console.log(y);
-console.log(typeof y);
+// console.log(y);
+// console.log(typeof y);
 
-let z = 1.5;
+// let z = 1.5;
 
-console.log(z);
-console.log(typeof z);
+// console.log(z);
+// console.log(typeof z);
 
-// boolean
+// // boolean
 
-let p = true;
-console.log(p);
-console.log(typeof p);
+// let p = true;
+// console.log(p);
+// console.log(typeof p);
 
-//undefined
+// //undefined
 
-let w;
-console.log(w);
-console.log(typeof w);
-w = 9;
-console.log(typeof w);
+// let w;
+// console.log(w);
+// console.log(typeof w);
+// w = 9;
+// console.log(typeof w);
 
-Null;
+// Null;
 
-let r = null;
+// let r = null;
 
-console.log(r);
+// console.log(r);
 
-console.log(typeof r);
+// console.log(typeof r);
 
-//Reference Types: Objects , Arrays , Functions
+// //Reference Types: Objects , Arrays , Functions
 
-let coursew = {
-  title: "Front End Development",
-  description: "Projects real time",
-  amout: 30000,
-  duration: {
-    html: "1 month",
-    css: "15 Days",
-    js: "1 month",
-    angular: "1 months",
-  },
-};
+// let coursew = {
+//   title: "Front End Development",
+//   description: "Projects real time",
+//   amout: 30000,
+//   duration: {
+//     html: "1 month",
+//     css: "15 Days",
+//     js: "1 month",
+//     angular: "1 months",
+//   },
+// };
 
-let xcourses = course;
+// let xcourses = course;
 
-console.log(course);
-console.log(typeof course);
+// console.log(course);
+// console.log(typeof course);
 
-console.log(course.title);
-console.log(course.description);
-console.log(course.amout);
-console.log(course.duration.css);
-console.log(course["duration"]["css"]);
+// console.log(course.title);
+// console.log(course.description);
+// console.log(course.amout);
+// console.log(course.duration.css);
+// console.log(course["duration"]["css"]);
 
-// Call By value
+// // Call By value
 
-let a = "Something";
+// let a = "Something";
 
-let b = a;
+// let b = a;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
-a = "Nothing";
+// a = "Nothing";
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
-// Call By Reference
+// // Call By Reference
 
-let course = {
-  title: "Front End Development",
-  description: "Projects real time",
-  amout: 30000,
-  duration: {
-    html: "1 month",
-    css: "15 Days",
-    js: "1 month",
-    angular: "1 months",
-  },
-};
+// let course = {
+//   title: "Front End Development",
+//   description: "Projects real time",
+//   amout: 30000,
+//   duration: {
+//     html: "1 month",
+//     css: "15 Days",
+//     js: "1 month",
+//     angular: "1 months",
+//   },
+// };
 
-let xcourse = course;
-console.log(course);
-console.log(xcourse);
+// let xcourse = course;
+// console.log(course);
+// console.log(xcourse);
 
-course.title = "BackEnd Developemnt"; // Mutate
+// course.title = "BackEnd Developemnt"; // Mutate
 
-console.log(course);
-console.log(xcourse);
+// console.log(course);
+// console.log(xcourse);
 
-function toChangeAmout(obj) {
-  // let ycourse = obj;
+// function toChangeAmout(obj) {
+//   // let ycourse = obj;
 
-  obj.amout = 40000;
-  console.log(obj.amout);
-  console.log(course);
-  console.log(xcourse);
-}
+//   obj.amout = 40000;
+//   console.log(obj.amout);
+//   console.log(course);
+//   console.log(xcourse);
+// }
 
-toChangeAmout(course);
-console.log(course);
-console.log(xcourse);
+// toChangeAmout(course);
+// console.log(course);
+// console.log(xcourse);
 
-course = {
-  title: "Xyz End Development",
-  description: "Projects real time",
-  amout: 30000,
-  duration: {
-    html: "1 month",
-    css: "15 Days",
-    js: "1 month",
-    angular: "1 months",
-  },
-};
+// course = {
+//   title: "Xyz End Development",
+//   description: "Projects real time",
+//   amout: 30000,
+//   duration: {
+//     html: "1 month",
+//     css: "15 Days",
+//     js: "1 month",
+//     angular: "1 months",
+//   },
+// };
 
-console.log(course);
-console.log(xcourse);
+// console.log(course);
+// console.log(xcourse);
+
+//How to create objectby using using Object Literal Syntax:
+
+//var objectName={};
+
+//How to create object by using new object keyword:
+
+var person = new Object();
+
+console.log(person);
+
+person.firstname='shaik';
+person.lastname='shabbeer';
+
+console.log(person);
+
+var firstNameProperty = 'firstname';
+
+var lastNameProperty='lastname';
+
+
+console.log(person[firstNameProperty]);
+console.log(person[lastNameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+
+
+
+
 
 
 
